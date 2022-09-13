ac14e96c496d1b11acb9e45d8bf519f66966668b
Maksim Fesenko <drresist@gmail.com>
2022-09-13 23:36:26 +0300

    task3 - add format optional arg

--- hexlet-code/scripts/hexlet-code.py
+++ hexlet-code/scripts/hexlet-code.py
@@ -14,6 +14,7 @@ def create_parser():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
+    parser.add_argument('-f', '--format', metavar='FORMAT', type=str, required=False, help='set format of output')
     return parser
 
 if __name__ == "__main__":
