13e9bc6d704326cc3e59025d0fd42b3b9db9b3a8
Maksim Fesenko <drresist@gmail.com>
2022-11-06 18:33:17 +0300

    run linter + add black

--- hexlet-code/scripts/hexlet-code.py
+++ hexlet-code/scripts/hexlet-code.py
@@ -1,23 +1,31 @@
 import argparse
 
+
 def main():
     """
     Enter point of application
     """
     parser = create_parser()
-    args = parser.parse_args()
     print("Initial template")
 
 
-def create_parser():
+def create_parser()->None:
     """Create argument parser for input files."""
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', metavar='first_file', type=str)
-    parser.add_argument('second_file', metavar='second_file', type=str)
-    parser.add_argument('-f', '--format', metavar='FORMAT', type=str, required=False, help='set format of output')
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference."
+    )
+    parser.add_argument("first_file", metavar="first_file", type=str)
+    parser.add_argument("second_file", metavar="second_file", type=str)
+    parser.add_argument(
+        "-f",
+        "--format",
+        metavar="FORMAT",
+        type=str,
+        required=False,
+        help="set format of output",
+    )
     return parser
+
 
 if __name__ == "__main__":
     main()
-
-
